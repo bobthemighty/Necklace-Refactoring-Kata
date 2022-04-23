9a2feff8bdbce499395ae839fe92998107269e79
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-04-23 10:48:01 +0100

    Add snapshot test for packNecklace

--- typescript/src/index.spec.ts
+++ typescript/src/index.spec.ts
@@ -1,8 +1,34 @@
-import { makeRing } from "./jewellery";
+import { makeStorage, packNecklace } from ".";
+import { Jewellery, makeNecklace, makePendantNecklace } from "./jewellery";
 
-describe("The packer", () => {
-  it("When packing a ring", () => {
-    const ring = makeRing("Amber");
-    expect(ring.size()).toBe("Small");
+const nameOf = (item: Jewellery) => {
+  switch (item._kind) {
+    case "Ring":
+      return `${item.stone} ring`;
+    case "Earring":
+      return `${item.stone} ${item.type} earrings`;
+    case "Necklace":
+      if (item.type === "Pendant") {
+        return `${item.chain.type} necklace with ${item.pendant.stone} pendant`;
+      }
+      return `${item.stone} ${item.type} necklace`;
+  }
+};
+
+const packNecklaceCases = [
+  makeNecklace("Diamond", "Chain"),
+  makeNecklace("Plain", "LongChain"),
+  makeNecklace("Amber", "Chain"),
+  makeNecklace("Pearl", "Beads"),
+  makePendantNecklace("Pearl", "Beads"),
+  makePendantNecklace("Diamond", "Chain"),
+  makePendantNecklace("Diamond", "LongChain"),
+];
+
+for (const item of packNecklaceCases) {
+  test(`Pack necklace: ${nameOf(item)}`, () => {
+    const storage = makeStorage();
+    packNecklace(item, storage);
+    expect(storage).toMatchSnapshot(nameOf(item));
   });
-});
+}
