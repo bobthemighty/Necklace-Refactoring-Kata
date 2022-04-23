fbbb28845ae7c0433eee3a026aab13cec9223b50
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-04-23 12:11:37 +0100

    Introduced "packer" type with pick/pack methods

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,36 +5,51 @@ import {
   PendantNecklace,
 } from "./jewellery";
 
-const safePacker = (storage: JewelleryStorage) => (item: Jewellery) => {
-  if (item.stone === "Diamond") {
-    storage.safe.push(item);
-    return true;
-  }
-};
+interface Packer<T extends Jewellery> {
+  pick(item: Jewellery): item is T;
+  pack(item: T): void;
+}
 
-const smallItemPacker = (storage: JewelleryStorage) => (item: Jewellery) => {
-  if (item.size() === "Small") {
-    storage.box.topShelf.push(item);
-    return true;
-  }
-};
+const safePacker = (storage: JewelleryStorage): Packer<Jewellery> => ({
+  pick(item: Jewellery): item is Jewellery {
+    return item.stone === "Diamond";
+  },
+  pack: (item: Jewellery) => storage.safe.push(item),
+});
 
-const defaultPacker = (storage: JewelleryStorage) => (item: Jewellery) => {
-  if (item.type === "Pendant") {
-    storage.tree.push(item.chain);
-    storage.box.topShelf.push(item.pendant);
-  } else {
-    storage.tree.push(item);
-  }
-};
+const smallItemPacker = (storage: JewelleryStorage): Packer<Jewellery> => ({
+  pick(item: Jewellery): item is Jewellery {
+    return item.size() === "Small";
+  },
+  pack: (item: Jewellery) => storage.box.topShelf.push(item),
+});
+
+const defaultNecklacePacker = (
+  storage: JewelleryStorage
+): Packer<Necklace> => ({
+  pick: (item: Jewellery): item is Necklace => item._kind === "Necklace",
+  pack: (item) => storage.tree.push(item),
+});
+
+const pendantNecklacePacker = (
+  storage: JewelleryStorage
+): Packer<PendantNecklace> => ({
+  pick: (x): x is PendantNecklace =>
+    x._kind === "Necklace" && x.type === "Pendant",
+  pack: (x) => {
+    storage.tree.push(x.chain);
+    storage.box.topShelf.push(x.pendant);
+  },
+});
 
 export function packNecklace(
   item: Necklace | PendantNecklace,
   storage: JewelleryStorage
 ) {
-  [safePacker(storage), smallItemPacker(storage), defaultPacker(storage)].find(
-    (pack) => pack(item)
-  );
+  [safePacker, smallItemPacker, pendantNecklacePacker, defaultNecklacePacker]
+    .map((p) => p(storage))
+    .find((p) => p.pick(item))
+    .pack(item);
 }
 
 export function pack(item: Jewellery, storage: JewelleryStorage) {
